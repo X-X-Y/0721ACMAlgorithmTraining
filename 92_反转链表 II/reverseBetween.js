4b284847e8ab3ba0a4b4e73d788cd2f371bd6633
X-X-Y <xxy3002@gmail.com>
2022-02-27 22:05:11 +0800

    22-02-27

--- 92_反转链表 II/reverseBetween.js
+++ 92_反转链表 II/reverseBetween.js
@@ -27,10 +27,36 @@ var reverseRight = function(head, right) {
 
 var reverseBetween = function(head, left, right) {
     // 递归
-    if(left == 1){
-        return reverseRight(head, right);
+    // if(left == 1){
+    //     return reverseRight(head, right);
+    // }
+    // // head.next开始的链表前right项全部反转完
+    // head.next = reverseBetween(head.next, left-1, right-1);
+    // return head;
+
+    // 迭代
+    let newHead = new ListNode(0, head);
+    let pre = newHead;
+
+    // 找到第left-1个节点
+    for(let i = 1; i < left; i++){
+        pre = pre.next;
     }
-    // head.next开始的链表前right项全部反转完
-    head.next = reverseBetween(head.next, left-1, right-1);
-    return head;
+    let cur = pre.next;
+    
+    while(left < right){
+        let next = cur.next;
+        cur.next = cur.next.next;
+        next.next = pre.next;
+        pre.next = next;
+        left++;
+    }
+    return newHead.next;
 };
+
+
+
+
+
+
+
