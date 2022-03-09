754cbbf262c65255248200eeeea1fd4015951d6b
X-X-Y <xxy3002@gmail.com>
2022-03-09 22:17:03 +0800

    22-03-09

---
+++ 707_设计链表/MyLinkedList.js
@@ -0,0 +1,127 @@
+class Node{
+    constructor(val, prve=null, next=null){
+        this.val = val
+        this.prve = prve
+        this.next = next
+    }
+}
+
+var MyLinkedList = function() {
+    this._size = 0
+    this._head = null
+};
+
+/** 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    if(index < 0 || index >= this._size) return -1
+    let count = 0
+    let cur = this._head
+    while(count++ < index){
+        cur = cur.next
+    }
+    return cur.val
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    let node = new Node(val)
+    if(this._size == 0){
+        this._head = node
+        this._size++
+        return
+    }
+    node.next = this._head
+    this._head = node
+    node.next.prve = node
+    this._size++
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+    let node = new Node(val)
+    if(this._size == 0){
+        this._head = node
+    }else{
+        let cur = this._head
+        while(cur.next != null){
+            cur = cur.next
+        }
+        cur.next = node
+        node.prve = cur
+    }
+    this._size++
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    if(index <= 0){
+        this.addAtHead(val)
+        return
+    }else if(index > this._size){
+        return
+    }else if(index == this._size){
+        this.addAtTail(val)
+        return
+    }else{
+        let count = 0
+        let cur = this._head
+        let node = new Node(val)
+        while(count++ < index){
+            cur = cur.next
+        }
+        cur.prve.next = node
+        node.prve = cur.prve
+        cur.prve = node
+        node.next = cur
+        this._size++
+    }
+};
+
+/** 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    if(index < 0 || index >= this._size) return
+    if(this._size != 0 && index == 0){
+        // 删除头节点
+        this._head = this._head.next
+        this._size--
+        return
+    }
+
+    let count = 0
+    let cur = this._head
+    while(count++ < index){
+        cur = cur.next
+    }
+    cur.prve.next =  cur.next
+    if(cur.next){
+        // 防止当前节点是尾节点
+        cur.next.prve = cur.prve
+    }
+    this._size--
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
