1feba112ce83717b7f540bfb7eae586f8aec25cf
X-X-Y <xxy3002@gmail.com>
2022-03-26 19:31:45 +0800

    22-03-26

---
+++ 322_零钱兑换/coinChange.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = Array(amount+1).fill(amount+1)
+
+    dp[0] = 0
+    for(let i = 1; i <= amount; i++){
+        for(let j = 0; j < coins.length; j++){
+            if(i - coins[j] < 0){
+                continue
+            }
+            dp[i] = Math.min(dp[i], dp[i-coins[j]]+1)
+        }
+    }
+    
+    return (dp[amount] == amount+1) ? -1 : dp[amount]
+};
+
+// console.log(coinChange([2,5,10,1], 27))
+// console.log(coinChange([1], 0))
+// console.log(coinChange([1], 5))
