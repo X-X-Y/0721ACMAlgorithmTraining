40916ad14fa1b762e6dd4bf1e5eab61eba32f27b
X-X-Y <xxy3002@gmail.com>
2022-04-28 21:14:41 +0800

    22-04-28

--- 72_编辑距离/minDistance.js
+++ 72_编辑距离/minDistance.js
@@ -41,8 +41,8 @@ var minDistance = function(word1, word2) {
 
     let dp = new Array(m+1).fill(0).map(()=>new Array(n+1).fill(0))
     // base case
-    // 备忘录时 i j == -1，但是数组下标最小为0，于是定义了m+1,n+1的dp数组
-    // i和j都往后移了1
+    // 当其中一个为0，相当于是个空字符串，什么长度的字符串变成空都要其长度次数操作
+    // 也即相当于，其中某个字符串已经比较完了，剩下保持相同需要操作剩下长度的次数
     for(let i = 1; i <= m; i++){
         // 当j==0时，返回i
         dp[i][0] = i
@@ -57,7 +57,16 @@ var minDistance = function(word1, word2) {
             if(word1[i-1] == word2[j-1]){
                 dp[i][j] = dp[i-1][j-1]
             }else{
-                dp[i][j] = Math.min(dp[i][j-1]+1, dp[i-1][j]+1, dp[i-1][j-1]+1)
+                dp[i][j] = Math.min(
+                    // 插入一个数跟j位置匹配了，那么要保证[i, j-1]相同，因为插入已经
+                    // 跟j相同了，那么[i, j]就是相同了，下次从[i+1, j+1]开始比较
+                    dp[i][j-1]+1, 
+                    // 第i位置删掉，就要保证[i-1, j]相同，因为i已经没了，就相当于说
+                    // i+1之前都跟j相同了，下次可以从[i+1, j+1]开始比较
+                    dp[i-1][j]+1, 
+                    // 第i位置换掉，要保证[i-1, j-1]相同，因为换掉后，i, j位置已经相
+                    // 同了，下次可以从[i+1, j+1]开始比较
+                    dp[i-1][j-1]+1)
             }
         }
     }
@@ -65,10 +74,4 @@ var minDistance = function(word1, word2) {
     return dp[m][n]
 };
 
-console.log(minDistance("intention", "execution"))
-
-
-
-
-
-
+// console.log(minDistance("intention", "execution"))
