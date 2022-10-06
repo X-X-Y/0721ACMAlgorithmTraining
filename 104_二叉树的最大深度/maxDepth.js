d3eb0d2ab782beba12d39e1774897a8be657f238
X-X-Y <xxy3002@gmail.com>
2022-10-06 22:39:38 +0800

    dfs vs bfs

--- 104_二叉树的最大深度/maxDepth.js
+++ 104_二叉树的最大深度/maxDepth.js
@@ -10,39 +10,82 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    // ====== 回溯 ======
-    // let res = 0
-    // let depth = 0
+var maxDepth = function (root) {
+  // ====== 回溯 ======
+  // let res = 0
+  // let depth = 0
 
-    // function traverse(root){
-    //     if(root == null){
-    //         return
-    //     }
+  // function traverse(root){
+  //     if(root == null){
+  //         return
+  //     }
 
-    //     depth++
-    //     res = Math.max(res, depth)
-    //     traverse(root.left)
-    //     traverse(root.right)
-    //     depth--
-    // }
+  //     depth++
+  //     res = Math.max(res, depth)
+  //     traverse(root.left)
+  //     traverse(root.right)
+  //     depth--
+  // }
 
-    // traverse(root)
-    // return res
+  // traverse(root)
+  // return res
 
-    // ====== 动规 ======
-    function curMaxDepth(root){
-        if(root == null){
-            return 0
-        }
+  // ====== 动规 ======
+  // function curMaxDepth(root){
+  //     if(root == null){
+  //         return 0
+  //     }
 
-        let left = curMaxDepth(root.left)
-        let right = curMaxDepth(root.right)
+  //     let left = curMaxDepth(root.left)
+  //     let right = curMaxDepth(root.right)
 
-        return 1 + Math.max(left, right)
+  //     return 1 + Math.max(left, right)
+  // }
+
+  // return curMaxDepth(root)
+
+  // ====== bfs ======
+  // let queue = []
+  // let res = []
+  // if(!root){
+  //     return res
+  // }
+
+  // queue.push(root)
+  // let curDeep = 0
+  // while(queue.length){
+  //     curDeep++
+  //     let curLen = queue.length
+  //     for(let i = 0; i < curLen; i++){
+  //         let curNode = queue.shift()
+  //         if(curNode.left){
+  //             queue.push(curNode.left)
+  //         }
+  //         if(curNode.right){
+  //             queue.push(curNode.right)
+  //         }
+  //     }
+  // }
+  // return curDeep
+
+  // ====== dfs ======
+  let res = [];
+  let path = [];
+  if (!root) {
+    return 0;
+  }
+  path.push(root);
+  function backtrack(n) {
+    if (!path[path.length - 1]) {
+      res.push(path.length - 1);
+      return;
     }
-
-    return curMaxDepth(root)
+    for (let item of [n.left, n.right]) {
+      path.push(item);
+      backtrack(item);
+      path.pop();
+    }
+  }
+  backtrack(root);
+  return Math.max(...res);
 };
-
-
